fb14ed2ad4aada38dc27a5ba2545ed90038add3b
Sherlyne <sherlynea1994@hotmail.com>
2024-08-26 20:32:07 +0300

    functions lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,44 @@
 #!/usr/bin/env python3
+def my_function(param):
+    print("Running my_function")
+    return param + 1
+
+my_function_return_value = my_function(1)
+
+my_function_return_value
+
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Naureen")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
+greet_with_default("Sunny")
+    
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+x = add(1,2)
+print(x)
+    
 
 def halve(number):
-    pass
+     if (number != number):
+         return None
+     return number/2
+num = halve(100)
+print(num)
+
+
