c632b47e92b542eb9c68ee1f01bb558aed14c4ba
ShernOC <sherlynea1994@hotmail.com>
2024-11-26 16:05:41 +0300

    update

--- lib/functions.py
+++ lib/functions.py
@@ -3,6 +3,8 @@ def my_function(param):
     print("Running my_function")
     return param + 1
 
+my_function(50) # returns 51 
+
 my_function_return_value = my_function(1)
 
 my_function_return_value
@@ -11,20 +13,20 @@ my_function_return_value
 def greet_programmer():
     print("Hello, programmer!")
 
-greet_programmer()
+greet_programmer() # Hello, programmer! 
 
 
 def greet(name):
     print(f"Hello, {name}!")
 
-greet("Naureen")
+greet("Maureen") # returns Hello Maureen
 
-
+#|This is a default Arguments if no name is provided programmer will be provided. 
 def greet_with_default(name="programmer"):
     print(f"Hello, {name}!")
 
-greet_with_default()
-greet_with_default("Sunny")
+greet_with_default() # return Hello, programmer
+greet_with_default("Sunny") # returns Hello, Sunny 
     
 
 def add(num1, num2):
